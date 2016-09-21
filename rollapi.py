b2b8e3816ec971c9cea28bf4bd3716ff3efff3c1
Dan Langford <danlangford@ldschurch.org>
2016-09-21 16:34:14 -0600

    Deploy

--- rollapi.py
+++ rollapi.py
@@ -1,4 +1,10 @@
+import logging
+import dice
 from flask import Flask
+from flask import request
+from flask import json
+from flask import Response
+
 
 app = Flask(__name__)
 
@@ -6,6 +12,20 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/api/v1/roll/<roll_cmd>')
+def get_roll(roll_cmd):
+    return str(dice.roll(roll_cmd))
+
+@app.route('/api/v1/hipchat', methods=['POST'])
+def hipchat():
+    log = app.logger
+    log.debug( 'hi' )
+    log.debug(request.get_json())
+
+    log.debug(request.json)
+    return request.json['item']['message']['message']
 
 
 if __name__ == '__main__':
