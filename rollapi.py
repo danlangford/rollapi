a71a15891f67e1cfca43e5e64eb75d54659bc28a
Dan Langford <danlangford@ldschurch.org>
2016-09-22 09:31:17 -0600

    auto-sum lists and support user provided messages

--- rollapi.py
+++ rollapi.py
@@ -7,6 +7,8 @@ from flask import jsonify
 
 app = Flask(__name__)
 
+def _get_roll(roll_cmd):
+    return dice.roll(roll_cmd)
 
 @app.route('/')
 def hello_world():
@@ -15,16 +17,27 @@ def hello_world():
 
 @app.route('/api/v1/roll/<roll_cmd>')
 def get_roll(roll_cmd):
-    return str(dice.roll(roll_cmd))
+    return str(_get_roll(roll_cmd))
 
 @app.route('/api/v1/hipchat', methods=['POST'])
 def hipchat():
-    log = app.logger
     body = request.get_json()
-    command = body['item']['message']['message'].replace('/roll ','')
     from_name = body['item']['message']['from']['mention_name']
-    roll_result = get_roll(command)
-    message = '@{} rolling {} gets {}'.format(from_name, command, roll_result)
+    roll_syntax, _d, other_msg = body['item']['message']['message'].replace('/roll ','').partition(' ')
+
+    roll = _get_roll(roll_syntax)
+
+    if isinstance(roll, (list,tuple)) and not isinstance(roll, basestring):
+        roll_msg = '{} (Σ={})'.format(roll, sum(roll))
+    else:
+        roll_msg = str(roll)
+
+    if len(other_msg)>0:
+        fluff = 'says "{}" with'.format(other_msg)
+    else:
+        fluff = 'rolling {} gets'.format(roll_syntax)
+
+    message = '@{} {} {}'.format(from_name, fluff, roll_msg)
 
     return jsonify({'color':'black','message':message,'notify':False,'message_format':'text'})
 
