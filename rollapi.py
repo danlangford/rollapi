468ceaf09a14974047a774f42a5b9c232a1ac858
Dan Langford <danlangford@ldschurch.org>
2017-11-09 14:16:23 -0700

    some changes i found on my computer

--- rollapi.py
+++ rollapi.py
@@ -8,10 +8,6 @@ from flask import jsonify
 app = Flask(__name__)
 
 
-def _get_roll(roll_cmd):
-    return dice.roll(roll_cmd)
-
-
 @app.route('/')
 def hello_world():
     return 'an api around the "dice" python lib thrown together real quick by @danlangford'
@@ -22,17 +18,26 @@ def get_roll(roll_cmd):
     return str(_get_roll(roll_cmd))
 
 
+def _get_roll(roll_cmd):
+    return dice.roll(roll_cmd)
+
+
 @app.route('/api/v1/hipchat', methods=['POST'])
 def hipchat():
-
     body = request.get_json()
     from_name = body['item']['message']['from']['mention_name']
     slash_command = body['item']['message']['message'].strip()
+    roll_msg, other_msg, color = _hipchat(slash_command)
+    message = '@{} {} {}'.format(from_name, roll_msg, other_msg)
+    return jsonify({'color':color,'message':message,'notify':False,'message_format':'text'})
 
+
+def _hipchat(slash_command):
     color='yellow'
     other_msg=''
     try:
-        # partition #1 removes the command (/roll) and partition #2 isolates the roll syntax from any other message
+        # 1st partition() removes the command (/roll)
+        # 2nd partition() isolates the roll syntax from any other message
         roll_syntax, _d, other_msg = slash_command.partition(' ')[2].partition(' ')
 
         if roll_syntax.lower().strip() in ['help','']:
@@ -48,15 +53,13 @@ def hipchat():
 
     if isinstance(roll, (list,tuple)) and not isinstance(roll, basestring):
         if len(roll) == 1:
-            roll_msg = str(roll)
+            roll_msg = str(roll[0])
         else:
             roll_msg = '{} (Σ={})'.format(roll, sum(roll))
     else:
         roll_msg = str(roll)
 
-    message = '@{} {} {}'.format(from_name, roll_msg, other_msg)
-
-    return jsonify({'color':color,'message':message,'notify':False,'message_format':'text'})
+    return roll_msg, other_msg, color
 
 
 if __name__ == '__main__':
